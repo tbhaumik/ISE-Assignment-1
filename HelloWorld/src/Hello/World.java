0f5f55f76137b34c6a31a082ae56503fe8f137d7
T BHAUMIK <tirthankar.x.bhaumik@gmail.com>
2012-10-20 18:05:06 +0100

    First commit 20/10/2012 18:04 Saturday ISE Assignment 1 First commit

---
+++ HelloWorld/src/Hello/World.java
@@ -0,0 +1,13 @@
+package Hello;
+
+public class World {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("Example output for ISE Assignment 1!");
+
+	}
+
+}
