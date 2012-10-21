b87b35b961e15e318c08ebf31f4d28a1f2173e6c
T BHAUMIK <tirthankar.x.bhaumik@gmail.com>
2012-10-21 15:21:12 +0100

    Sunday 21/10/2012 15:20 This is my third commit for the ISE Assignment 1.

---
+++ Third-Project-ISE-Assignment-1/src/thirdPackage/thirdClass.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package thirdPackage;
+
+/**
+ * @author TBhaumik
+ *
+ */
+public class thirdClass {
+
+	public static void main(String[] args) {
+		System.out.print("This is the third java program");
+
+	}
+
+}
